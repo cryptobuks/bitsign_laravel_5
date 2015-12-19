fda2a57c7744f4844c29f1fce9783f62765f300f
iwalpola <iwalpola@gmail.com>
2015-12-19 19:00:09 +0530

    changed AuthController with additional validation and storage fields

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -42,9 +42,12 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
+            'name' => 'required|alpha_num|max:10',
             'email' => 'required|email|max:255|unique:users',
+            'f_name' => 'required|alpha|max:50',
+            'l_name' => 'required|alpha|max:50',
             'password' => 'required|confirmed|min:6',
+            'agree' => 'accepted',
         ]);
     }
 
@@ -58,6 +61,8 @@ class AuthController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'f_name' => $data['f_name'],
+            'l_name' => $data['l_name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
