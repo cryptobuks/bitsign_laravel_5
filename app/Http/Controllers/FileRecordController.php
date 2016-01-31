38839982b3f214f801d06e6f471422c6e4d9c62b
iwalpola <iwalpola@gmail.com>
2016-01-31 09:56:43 +0530

    bugfix on filerecord mimetype (check before encrypting!!!!!)

--- app/Http/Controllers/FileRecordController.php
+++ app/Http/Controllers/FileRecordController.php
@@ -115,7 +115,10 @@ class FileRecordController extends Controller
 	            $filename        = $salt.'_'.$original_name;
 	        	$uploadSuccess   = $upload->move($uploadPath, $filename);
 				if($uploadSuccess){
+					//hash
 				 	$shafile = base64_encode(hash_file('sha384', $uploadPath.'/'.$filename, true));
+				 	//get mimetype
+				 	$mimetype = finfo_file($finfo, $uploadPath.'/'.$filename);
 				 	//check whether file already exists for this contract
 				 	if ($currentfiles->where('hash', $shafile)->first()) {
 				 		$errors[] = 'File ' . $upload->getClientOriginalName() . ' has already been added to this contract';
@@ -132,7 +135,7 @@ class FileRecordController extends Controller
 				        $filerecord->filename = $original_name;
 				        $filerecord->salt = $salt;
 				        $filerecord->contract_id = $contract_id;
-				        $filerecord->type = finfo_file($finfo, $uploadPath.'/'.$filename);
+				        $filerecord->type = $mimetype;
 				        $filerecord->encrypted = true;
 				        $filerecord->save();
 						$files[] = 'File ' . $upload->getClientOriginalName() . ' successfully added as hash value: ' . $shafile ;
