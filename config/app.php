27717177a50e023fe13ed1224167b814b2dde00c
iwalpola <iwalpola@gmail.com>
2016-02-02 20:10:02 +0530

    bugfix

--- config/app.php
+++ config/app.php
@@ -136,6 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Iwalpola\XmlDigitalSignature\XmlDigitalSignatureServiceProvider::class,
 
         /*
          * Application Service Providers...
