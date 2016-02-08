2b648a5d60e779ae080d258eb10b30cdbece8b7b
iwalpola <iwalpola@gmail.com>
2016-02-08 22:36:39 +0530

    move tinymce init to contracts.create

--- public/js/bitsign.js
+++ public/js/bitsign.js
@@ -206,32 +206,6 @@ function DashboardTabChecker(){
 	});
 }
 //
-// Helper for run TinyMCE editor with textarea's
-//
-function TinyMCEStart(elem){
-	var plugins = [];
-	tinymce.init({selector: elem,
-		theme: "modern",
-		plugins: plugins,
-		//content_css: "css/style.css",
-		toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
-		style_formats: [
-			{title: 'Header 2', block: 'h2', classes: 'page-header'},
-			{title: 'Header 3', block: 'h3', classes: 'page-header'},
-			{title: 'Header 4', block: 'h4', classes: 'page-header'},
-			{title: 'Header 5', block: 'h5', classes: 'page-header'},
-			{title: 'Header 6', block: 'h6', classes: 'page-header'},
-			{title: 'Bold text', inline: 'b'},
-			{title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
-			{title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
-			{title: 'Example 1', inline: 'span', classes: 'example1'},
-			{title: 'Example 2', inline: 'span', classes: 'example2'},
-			{title: 'Table styles'},
-			{title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
-		]
-	});
-}
-//
 //  Helper for open ModalBox with requested header, content and bottom
 //
 //
