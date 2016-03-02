18718509e23213b6db510a68cd51427ec6526b8b
iwalpola <iwalpola@gmail.com>
2016-03-02 15:52:16 +0530

    deleting views

--- resources/views/common/head.blade.php
+++
@@ -1,7 +0,0 @@
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta charset="utf-8">
-<meta name="description" content="Create, Sign and Timestamp contracts with Bitcoin">
-<meta name="author" content="BitSign.it">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-<link href="/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
--- resources/views/dashboard/head.blade.php
+++
@@ -1,6 +0,0 @@
-<title>Dash - BitSign</title>
-<link href="plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet">
-<link href='http://fonts.googleapis.com/css?family=Righteous' rel='stylesheet' type='text/css'>
-<link href="plugins/fancybox/jquery.fancybox.css" rel="stylesheet">
-<link href="plugins/select2/select2.css" rel="stylesheet">
-<link href="css/bitsign_style.css" rel="stylesheet">
